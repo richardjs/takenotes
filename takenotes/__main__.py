a19b40bec15d58c89f3c1193646111f8f4464045
Richard Schneider <richard@schneiderbox.net>
2019-11-05 19:45:06 -0600

    work on not starting a song until a note is played

--- takenotes/__main__.py
+++ takenotes/__main__.py
@@ -17,10 +17,19 @@ def save_song(msgs):
     mid = mido.MidiFile()
     mid.ticks_per_beat = TICKS_PER_BEAT
 
-    last_time = msgs[0].time
+    first_time = None
     for msg in msgs:
-        ticks = int(mido.second2tick(
-            msg.time - last_time, TICKS_PER_BEAT, 500000))
+        if msg.type == 'note_on':
+            first_time = msg.time
+            break
+
+    last_time = first_time
+    for msg in msgs:
+        if msg.time < first_time:
+            t = 0
+        else:
+            t = msg.time - last_time
+        ticks = int(mido.second2tick(t, TICKS_PER_BEAT, 500000))
         last_time = msg.time
         msg.time = ticks
     track = mido.MidiTrack(msgs)
@@ -65,18 +74,28 @@ outport = mido.open_output(output_name)
 
 
 last_msg_time = time.time()
+song_started = False
 msgs = []
+notes_on = set()
 while 1:
-    if msgs and time.time() - last_msg_time > args.new_song_time:
+    if song_started and len(notes_on) == 0 and time.time() - last_msg_time > args.new_song_time:
         save_song(msgs)
         outport.send(mido.Message('note_on', note=96))
         outport.send(mido.Message('note_on', note=103))
         msgs = []
+        song_started = False
 
     for msg in inport.iter_pending():
         if msg.type == 'clock':
             continue
+        if msg.type == 'note_on':
+            song_started = True
+            notes_on.add(msg.note)
+        if msg.type == 'note_off' or (msg.type == 'note_on' and msg.velocity == 0):
+            notes_on.remove(msg.note)
         print(msg)
+        print(notes_on)
         msg.time = time.time()
         msgs.append(msg)
+        print(len(msgs))
         last_msg_time = time.time()
